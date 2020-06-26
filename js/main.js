db56d0e36071e200fa77c6fb3cf0b3daf3888dcc
LuayAlali <Luayalali@gmail.com>
2020-06-25 18:58:53 -0700

    created a search input instead of plugin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-$('#search').hideseek({
-	attribute: 'title'
-	
+$(document).ready(function(){
+	let images = document.getElementsByTagName('a');
+	$("#search").on('keyup', function () {
+	let search = $('#search').val().toLowerCase();
+	for ( let i = 0; i < images.length; i++){
+		let searchVal = images[i].getAttribute('data-caption');
+		if (searchVal.toLowerCase().indexOf(search) > -1) {
+			images[i].style.display = "";
+			} else {
+			images[i].style.display = "none";
+			}
+			
+	}
+					});
 });
+	
 
+
+
+
+
+$.fancybox.defaults.animationEffect = "zoom";
+$.fancybox.defaults.transitionEffect = "zoom-in-out";
 
